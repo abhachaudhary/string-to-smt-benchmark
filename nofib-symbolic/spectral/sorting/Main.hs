a75929161adab6db92f15d6b32b270d88fcaffd3
Abha Chaudhary <abha.bittu@gmail.com>
2025-07-15 14:59:36 -0400

    Added nofib benchmarks that operate on strings

---
+++ nofib-symbolic/spectral/sorting/Main.hs
@@ -0,0 +1,26 @@
+module Main2(main) where
+
+import Sort
+
+import Control.Monad (replicateM_)
+import Data.List (intersperse)
+import System.Environment (getArgs)
+import NofibUtils (hash)
+
+main f = mangle f
+
+mangle :: String{-input to sort-} -> String{-output-}
+mangle inpt
+  = (unlines . sort . lines) inpt
+  where
+    sort = foldr (.) id (intersperse reverse sorts)
+    sorts =
+      [ heapSort
+      , insertSort
+      , mergeSort
+      , quickSort
+      , quickSort2
+      , quickerSort
+      , treeSort
+      , treeSort2
+      ]
